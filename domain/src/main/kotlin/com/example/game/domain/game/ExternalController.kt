026e361c0418004c12b7778daf794fba7cfbaef6
izgib <sasha.cheremisov@gmail.com>
2020-08-25 22:36:04 +0300

    External Controller External Controller is game controller implementation that is based on getting info outside and change state of based on received info.

---
+++ domain/src/main/kotlin/com/example/game/domain/game/ExternalController.kt
@@ -0,0 +1,12 @@
+package com.example.game.domain.game
+
+
+class ExternalController(rows: Int, cols: Int, win: Int) : GameController(rows, cols, win) {
+    fun sendState(state: GameState) {
+        when (state) {
+            is Continues -> turn++
+            else -> {
+            }
+        }
+    }
+}
