48d78a28f57cfd4dd4200bd366b17aa9c3c1e3d9
izgib <sasha.cheremisov@gmail.com>
2022-01-29 20:57:54 +0300

    create buildSrc module for sharing library versions

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    //implementation(kotlin("gradle-plugin"))
+    //implementation(kotlin("stdlib"))
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,18 @@
+object Versions {
+    const val kotlin = "1.5.31"
+    const val coroutines = "1.5.1"
+
+    const val junit5 = "5.7.1"
+    const val junit4 = "4.13.2"
+
+    const val andrTest = "1.4.0"
+    const val andrTestExt = "1.1.3"
+    const val andrArchCore = "2.1.0"
+}
+
+object AndroidVers {
+    const val sdk = 29
+    //const val sdkBuild = "29.0.3"
+    const val sdkBuild = "30.0.3"
+    const val gradleVersion = "4.2.2"
+}
